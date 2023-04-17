c2ad53bc881637076bb3d58970006bba2a81b86b
Jayashre ‎ <128211962+fromjyce@users.noreply.github.com>
2023-04-17 13:44:59 +0530

    Create MaxofTwoNos.py

---
+++ LabRecords/MaxofTwoNos.py
@@ -0,0 +1,7 @@
+#Find Maximum of two numbers
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+if a>b:
+  print(str(a) + " is the maximum of  " + str(a) + " and " + str(b))
+else:
+  print(str(b) + " is the maximum of " + str(a) + " and " + str(b))
