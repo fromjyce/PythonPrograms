22bb2ba6c18f1de0d8dab8af19258015ade6f274
Jayashre ‎ <128211962+fromjyce@users.noreply.github.com>
2023-05-11 23:57:06 +0530

    Add files via upload

---
+++ Assignments/VolumeOfSphere.py
@@ -0,0 +1,4 @@
+import math
+rad = int(input("Enter the radius of the sphere: "))
+vol = (4*(math.pi)*rad**3)/3
+print(str(vol) + " sq.units is the volume of the sphere with radius " + str(rad) + " units")
