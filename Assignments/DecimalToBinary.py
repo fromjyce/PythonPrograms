866012f3edb1f1555b282a967e03e84486c15f4e
Jayashre ‎ <128211962+fromjyce@users.noreply.github.com>
2023-05-23 12:13:54 +0530

    Add files via upload

--- Assignments/DecimalToBinary.py
+++ Assignments/DecimalToBinary.py
@@ -1,4 +1,13 @@
-#Program that converts a decimal to its binary equivalent
-import math
-num = int(input("Enter the number: "))
-print("The Binary Equivalent is " + str(num) + " is " + str(bin(num).replace("0b"," ")))
+'''
+Write a Python program to find the binary equivalent of a number using recursion.
+'''
+
+def decimal_to_binary(n):
+    if n > 1:
+        decimal_to_binary(n // 2)
+    print(n % 2, end='')
+
+
+decimal_number = int(input("Enter a decimal number: "))
+print("Binary equivalent:", end=' ')
+decimal_to_binary(decimal_number)
