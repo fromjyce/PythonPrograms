8a19ce153dc2f939e2b2c6c45830d6b2a53783ed
Jayashre ‎ <128211962+fromjyce@users.noreply.github.com>
2023-08-01 22:26:05 +0530

    Create FindMaxSum.py

---
+++ PythonPrograms/FindMaxSum.py
@@ -0,0 +1,13 @@
+#Give an integer list, return the maxikum sublist sum. the list may conatin  both positive and negative integers and is unsorted
+def max_sublist_sum(lst):
+    if not lst:
+        return 0
+
+    current_sum = lst[0]
+    max_sum = lst[0]
+
+    for num in lst[1:]:
+        current_sum = max(num, current_sum + num)
+        max_sum = max(max_sum, current_sum)
+
+    return max_sum
